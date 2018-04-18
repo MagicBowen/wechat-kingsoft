09afb1b2e0c8b170810b2919018559b55b7ae608
Bowen <e.wangbo@aliyun.com>
2018-04-18 17:58:13 +0800

    add more

---
+++ config.py
@@ -0,0 +1,7 @@
+TOCKEN = 'test'
+APP_ID = 'wxf164ebf08b919a0f'
+APP_SECRET = 'ddb3252ecbcb79f47a44f1a049013546'
+ENCRYPT = False
+ENCODING_AES_KEY ='KD4kyN1crLqOaRf8QpkfOTHhd2lscisWBZAiAhf4IbY'
+CHATBOT_URL = 'http://127.0.0.1:8080/query'
+HTTP_PORT = 443
